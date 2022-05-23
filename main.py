59254a8a3de031d37926188d890a7180000a5969
Nikshiptha <nikshiptha2002@gmail.com>
2022-05-23 13:34:17 +0530

    commit

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from pydoc import render_doc
-from flask import Flask, render_template,redirect,url_for
+from flask import Flask, render_template,redirect, request,url_for,request
+from flask_mysqldb import MySQL
 app=Flask(__name__)
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
+    if request.method == 'POST':
+        userDetails=request.form
+        email=userDetails['name']
+        password=userDetails['password']
     return render_template("login.html")
 @app.route('/register')
 def new_user():
