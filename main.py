7ccf13ea9c8032543f668ef04944fb595e11e551
Nikshiptha <nikshiptha2002@gmail.com>
2022-06-04 12:13:58 +0530

    commit

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import mysql.connector
 import yaml
 app=Flask(__name__)
 #Configure the database
-con=mysql.connector.connect(host='localhost',user='root',password='',database='cartoonify')
-cur=con.cursor()
 """
 app.config['MYSQL_HOST']=''
 app.config['MYSQL_USER']=''
@@ -26,38 +24,50 @@ def register():
 def home():
     return render_template('home.html')
 """
-@app.route('/loginvalidation',methods=['POST','GET'])
+@app.route('/loginvalidation',methods=['POST'])
 def loginvalidation():
     email=request.form.get('email')
     password=request.form.get('password')
-    cur.execute("""SELECT * from 'user' where 'email' like '()' and 'password' like '()'""",format(email,password))
+    con=mysql.connector.connect(host='localhost',user='root',password='')
+    cur=con.cursor()
+    cur.execute("use cartoonify;")
+    res="select * from users where email=email and password=password;"
+    cur.execute(res)
     count=cur.fetchall()
+    cur.close()
     if len(count)>0:
         return render_template("edit.html")
     else:
         return render_template("register.html")
-
+    
 
 
 @app.route('/newuser',methods=['POST'])
 def new_user():
     profpic=request.form.get('profpic')
     name=request.form.get('uname')
+    print(name)
     email=request.form.get('email')
     password=request.form.get('pass')
     con_passsword=request.form.get('confirmpass')
     gender=request.form.get('gender')
     phno=request.form.get('phno')
-    cur.execute("SELECT * from user where email=%s and password =%s",(email,password))
+    con=mysql.connector.connect(host='localhost',user='root',password='')
+    cur=con.cursor()
+    cur.execute("use cartoonify;")
+    res="select * from users where email=email and password=password;"
+    cur.execute(res)
     count=cur.fetchall()
     if len(count)>0:
         flash('User already exists.Register again!')
+        cur.close()
         return render_template("register.html")
     else:
-        cur.execute("INSERT INTO user(id,name,password,gender,phno,profpic) values(%s,%s,%s,%s,%d,%s)",(name,email,password,gender,phno,profpic))
+        res="insert into users(id,name,email,password,gender,phno,profpic) values(default,'{}','{}','{}','{}','{}','{}')".format(name,email,password,gender,phno,profpic)
+        cur.execute(res)
         con.commit()
         cur.close()
-        flash('User added successfully')
+        """flash('User added successfully')"""
         return render_template("login.html")
 if __name__=='__main__':
     app.run(debug=True)
