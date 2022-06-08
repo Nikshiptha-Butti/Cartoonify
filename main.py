39ba918b20de80ddc0d07b4ded88ef49dcea7674
Nikshiptha <nikshiptha2002@gmail.com>
2022-06-08 23:16:51 +0530

    commit

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 from pydoc import render_doc
-from flask import Flask, render_template,redirect, request,url_for,request,flash
+from flask import Flask, render_template,redirect, request,url_for,flash
 from flask_mysqldb import MySQL
 import mysql.connector
 import notifypy
 import cv2
-notification=notifypy.Notify();
+notification=notifypy.Notify()
 import yaml
+import os
+from werkzeug.utils import secure_filename
 app=Flask(__name__)
 #Configure the database
-"""
-app.config['MYSQL_HOST']=''
-app.config['MYSQL_USER']=''
-app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']=''
-mysql=MySQL(app)*/
-"""
 @app.route('/')
 def login(methods=['POST']):   
     return render_template("login.html")
@@ -27,6 +22,7 @@ def register():
 def home():
     return render_template('home.html')
 """
+email=""
 @app.route('/loginvalidation',methods=['POST'])
 def loginvalidation():
     global email
@@ -56,7 +52,6 @@ def loginvalidation():
 def new_user():
     profpic=request.form.get('profpic')
     name=request.form.get('uname')
-    print(name)
     email=request.form.get('email')
     password=request.form.get('pass')
     con_passsword=request.form.get('confirmpass')
@@ -88,25 +83,19 @@ def new_user():
 
 @app.route('/edit',methods=['POST'])
 def edit_photos():
-    return render_template("edit.html")
-    """count=0
+    print(email)
+    count=0
     grey_scale=request.form.get('grey_scale')
     mean_blur=request.form.get('mean_blur')
     median_blur=request.form.get('median_blur')
     guassian_blur=request.form.get('guassian _blur')
     neural_style=request.form.get('neural_style')
-    img=request.form.get('photo')
+    filename=request.files.getlist('pic')
     con=mysql.connector.connect(host='localhost',user='root',password='')
     cur=con.cursor()
     cur.execute("use cartoonify;")
-    id="select id from users where email='{}'".format(email)
-    photos=[]
-    print(grey_scale)
-    if(grey_scale is not None):
-        gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        photos[count]=gray
-        count=count+1
-        print(grey_scale)"""
+
+    return render_template("edit.html")
     
 if __name__=='__main__':
     app.run(debug=True)
