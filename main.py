e3878b82d00aa33a69cccf981537569ddadafaf4
Nikshiptha <nikshiptha2002@gmail.com>
2022-06-10 22:43:36 +0530

    commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pydoc import render_doc
+from cv2 import VideoCapture
 from flask import Flask, render_template,redirect, request,url_for,flash,session
 from flask_mysqldb import MySQL
 import mysql.connector
@@ -47,13 +48,22 @@ def loginvalidation():
         notification.message="Login successful!"
         notification.icon="./static/images/logo.jpg"
         notification.send()
-        return render_template('edit.html')
+        return render_template('select.html')
     else:
         notification.title="Cartoonify"
         notification.message="No such user found. Register Now!"
         notification.icon="./static/images/logo.jpg"
         notification.send()
         return render_template("register.html")
+
+@app.route('/uploadpic',methods=['POST'])
+def uploadpic():
+    return render_template("edit.html")
+
+@app.route('/capturepic',methods=['POST'])
+def capturepic():
+    print('me')
+    return render_template("video.html")
 
 @app.route('/newuser',methods=['POST'])
 def new_user():
@@ -88,8 +98,24 @@ def new_user():
         notification.send()
         return render_template("login.html")
 
-@app.route('/edit',methods=['POST'])
-def edit():
+@app.route('/apply',methods=['POST'])
+def apply():
+    cam_port=0
+    print('me')
+    cam=VideoCapture(cam_port)
+    result, image = cam.read()
+    if result:
+        print('me')
+        vimg=Image.fromarray(image)
+        vimg.save('static/uploads/newvimg.jpeg')
+    else:
+        print("No image detected. Please! try again")
+    return render_template("video.html")
+    
+
+
+@app.route('/video',methods=['POST'])
+def video():
     count=0
     global email
     flname=""
@@ -99,14 +125,10 @@ def edit():
     guassian_blur=request.form.get('guassian_blur')
     bilateral_filter=request.form.get('b_filter')
     color_palatte=request.form.get('color_palatte')
-    neural_style=request.form.get('neural_style')
-    img=request.files.getlist('pic')
-    for f in img:
-        flname=secure_filename(f.filename)
-        fos=os.path.join(app.config['UPLOAD_FOLDER'],flname)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'],flname))
-        flname=UPLOAD_FOLDER+flname
+    neural_style=request.form.get('neural_style')    
+    flname=UPLOAD_FOLDER+'newvimg.jpeg'
     print(flname)
+    send=[]
     con=mysql.connector.connect(host='localhost',user='root',password='')
     cur=con.cursor()
     cur.execute("use cartoonify;")
@@ -115,6 +137,88 @@ def edit():
         gray=cv2.cvtColor(imgread,cv2.COLOR_BGR2GRAY)
         graynew=Image.fromarray(gray)
         graynew.save('static/uploads/newgray.jpeg')
+        send[count]="static/uploads/newgray.jpeg"
+        count=count+1
+    if(mean_blur is not None):
+        imgread=Image.open(flname)
+        blurimg=imgread.filter(ImageFilter.BLUR)
+        blurimg.save('static/uploads/blurnew.jpeg')
+        send[count]="static/uploads/blurnew.jpeg"
+        count=count+1
+    if(guassian_blur is not None):
+        imgread=Image.open(flname)
+        guassianimg=imgread.filter(ImageFilter.GaussianBlur(5))
+        guassianimg.save('static/uploads/guassian.jpeg')
+        send[count]="static/uploads/guassian.jpeg"
+        count=count+1
+    if(median_blur is not None):
+        read=cv2.imread(flname)
+        medianblur=cv2.medianBlur(read,5)
+        medianblur=Image.fromarray(medianblur)
+        medianblur.save('static/uploads/medianblur.jpeg')
+        send[count]="static/uploads/medianblur.jpeg"
+        count=count+1
+    if(bilateral_filter is not None):
+        read=cv2.imread(flname)
+        b_filter=cv2.bilateralFilter(read,d=7,sigmaColor=200,sigmaSpace=200)
+        b_filter=Image.fromarray(b_filter)
+        b_filter.save('static/uploads/bfilter.jpeg')
+        send[count]="static/uploads/bfilter.jpeg"
+        count=count+1
+    if(color_palatte is not None):
+        read=cv2.imread(flname)
+        # Defining input data for clustering
+        data = np.float32(read).reshape((-1, 3))
+        # Defining criteria
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 1.0)
+        # Applying cv2.kmeans function
+        ret, label, center = cv2.kmeans(data,3, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+        center = np.uint8(center)
+        result = center[label.flatten()]
+        result = result.reshape(read.shape)
+        img=Image.fromarray(result)
+        img.save('static/uploads/colorpalatte.jpeg')
+        send[count]="static/uploads/colorpalatte.jpeg"
+        count=count+1
+    return render_template("video.html",send=send,count=count)
+
+
+@app.route('/edit',methods=['POST'])
+def edit():
+    count=0
+    global email
+    flname=""
+    name=""
+    grey_scale=request.form.get('grey_scale')
+    mean_blur=request.form.get('mean_blur')
+    median_blur=request.form.get('median_blur')
+    guassian_blur=request.form.get('guassian_blur')
+    bilateral_filter=request.form.get('b_filter')
+    color_palatte=request.form.get('color_palatte')
+    neural_style=request.form.get('neural_style')
+    img=request.files.getlist('pic')
+    print(img)
+    print('me')
+    if not img:
+        flname=UPLOAD_FOLDER+'newvimg.jpeg'
+        print(flname)
+    else:
+        for f in img:
+            flname=secure_filename(f.filename)
+            fos=os.path.join(app.config['UPLOAD_FOLDER'],flname)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'],flname))
+            flname=UPLOAD_FOLDER+flname
+        print(flname)
+    sendp=[]
+    con=mysql.connector.connect(host='localhost',user='root',password='')
+    cur=con.cursor()
+    cur.execute("use cartoonify;")
+    if(grey_scale is not None):
+        imgread=cv2.imread(flname)
+        gray=cv2.cvtColor(imgread,cv2.COLOR_BGR2GRAY)
+        graynew=Image.fromarray(gray)
+        graynew.save('static/uploads/newgray.jpeg')
+        sendp[count]="static/uploads/guassian.jpeg"
         count=count+1
     if(mean_blur is not None):
         imgread=Image.open(flname)
